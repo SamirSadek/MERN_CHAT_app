f11975d408280a804441abdf87202121a163b1f8
Samir Sadek <samirsadek2009@gmail.com>
2024-09-25 19:52:59 +0600

    signup,login,logout endpoint with jwt

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,16 +1,80 @@
-export const signup = (req, res) =>{
-    
-    try {
-        const {fullName, userName, password, confirmPassword, gender}= req.body
-    } catch (error) {
-        
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/genToken.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, userName, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords does not match" });
     }
-}
-export const login = (req, res) =>{
-    console.log('login');
-}
-export const logout = (req, res) =>{
-    console.log('logout');
-}
 
+    const user = await User.findOne({ userName });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/boy?username=${userName}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/girl?username=${userName}`;
+
+    const newUser = new User({
+      fullName,
+      userName,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        userName: newUser.userName,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+export const login = async(req, res) => {
+  try {
+    const {userName, password} = req.body
+    const user = await User.findOne({userName})
+    const isPassCorrect = await bcrypt.compare(password, user?.password || "")
+
+    if(!user || !isPassCorrect){
+         return res.status(400).json({ error: "Invalid username or password"})
+    }
+    generateTokenAndSetCookie(user._id, res)
+
+    res.status(201).json({
+        _id: user._id,
+        fullName: user.fullName,
+        userName: user.userName,
+        profilePic: user.profilePic,
+      });
+
+  } catch (error) {
+    console.log("Error in login controller ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+export const logout = (req, res) => {
+  try {
+    res.cookie("jwt", "" , {maxAge: 0})
+    res.status(200).json({message: "Logged out Successfully"})
+  } catch (error) {
+    console.log("Error in login controller ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
+dotenv.config()
 import authRoutes from './routes/auth.routes.js'
 import connectMongo from './db/connectMongo.js'
 
 const app = express()
-dotenv.config()
 const PORT = process.env.PORT || 6000
+
+
+
+app.use(express.json())
+
+app.use('/api/auth',authRoutes)
+
 
 
 app.get('/',(req,res)=>{
     res.send("Server is Running!!")
 })
-
-app.use('/api/auth',authRoutes)
 
 app.listen(PORT, ()=>{
     connectMongo()

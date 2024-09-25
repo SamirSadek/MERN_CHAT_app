31970669132af8a884a291feba0346cf12242d7f
Samir Sadek <samirsadek2009@gmail.com>
2024-09-26 02:09:31 +0600

    create get msg and get users endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
 dotenv.config()
+
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+
 import connectMongo from './db/connectMongo.js'
 
 const app = express()
 const PORT = process.env.PORT || 6000
 
-
-
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth',authRoutes)
+app.use('/api/messages',messageRoutes)
+app.use('/api/users',userRoutes)
 
 
 

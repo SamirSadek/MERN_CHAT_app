361ac0fcf52dbdd9e1c0ff69ab824b81d0b956f1
Samir Sadek <samirsadek2009@gmail.com>
2024-09-27 18:40:25 +0600

    frontend part done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
 
+import {  Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
+import SignUp from "./pages/signup/SignUp";
+import Login from "./pages/login/Login";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 
 function App() {
+  const { authUser } = useAuthContext();
 
   return (
     <div className='p-4 h-screen flex items-center justify-center bg-gray-400'>
-      {/* <Login/>
-      <SignUp/> */}
-      <Home/>
+      <Routes>
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+			</Routes>
+      <Toaster/>
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
